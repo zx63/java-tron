e715171f5715b73d492510a3b45535c337638208
ashu <ithinker1991@gmail.com>
2018-05-15 13:39:22 +0800

    tmp:server busy

--- src/main/java/org/tron/core/Wallet.java
+++ src/main/java/org/tron/core/Wallet.java
@@ -48,7 +48,6 @@ import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.capsule.WitnessCapsule;
 import org.tron.core.db.AccountStore;
 import org.tron.core.db.Manager;
-import org.tron.core.db.PendingManager;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
 import org.tron.core.exception.DupTransactionException;
@@ -225,7 +224,8 @@ public class Wallet {
 //        return builder.setResult(false).setCode(response_code.SERVER_BUSY).build();
 //      }
 
-      else {
+//      else{
+      {
         dbManager.pushTransactions(trx);
         p2pNode.broadcast(message);
         return builder.setResult(true).setCode(response_code.SUCCESS).build();
